6a9fec610ba1c008b0916b67cb30ba7a49b53868
Liqun Li <liqul@outlook.com>
2022-03-30 11:27:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,9 @@
 echo "Updating packages ..."
 apt update
 apt upgrade -y
-apt install tree -y
+
+# setup a cron job
+crontab -l > cron_bkp
+echo "0 10 * * * sh /home/test.sh >/dev/null 2>&1" >> cron_bkp
+crontab cron_bkp
+rm cron_bkp
