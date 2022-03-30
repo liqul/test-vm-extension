725ebf7aa00882e9d934c560ffa6c706ecf17d6b
Liqun Li <liqul@outlook.com>
2022-03-30 14:41:22 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,13 @@ echo "Updating packages ..."
 apt update
 apt upgrade -y
 
+# download the monitor script
+mkdir /monitor
+cd /monitor
+wget https://raw.githubusercontent.com/liqul/test-vm-extension/main/monitor.py --directory-prefix=/monitor/
+
 # setup a cron job
 crontab -l > cron_bkp
-echo "0 10 * * * sh /home/test.sh >/dev/null 2>&1" >> cron_bkp
+echo "0 10 * * * python3 monitor.py >/dev/null 2>&1" >> cron_bkp
 crontab cron_bkp
 rm cron_bkp
