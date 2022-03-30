7df0f91ae2728c80114a2388cf6f1d2e6c57f5de
Liqun Li <liqul@outlook.com>
2022-03-30 14:33:33 +0800

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import json
+import socket
+import urllib.request
+
+metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01"
+this_host = socket.gethostname()
+
+
+def get_scheduled_events():
+    headers = {"Metadata":"true"}
+    req = urllib.request.Request(url = metadata_url, headers=headers)
+    resp = urllib.request.urlopen(req)
+    data = json.loads(resp.read())
+    return data
+
+
+def handle_scheduled_events(data):
+    for evt in data['Events']:
+        eventid = evt['EventId']
+        status = evt['EventStatus']
+        resources = evt['Resources']
+        eventtype = evt['EventType']
+        resourcetype = evt['ResourceType']
+        notbefore = evt['NotBefore'].replace(" ", "_")
+        description = evt['Description']
+        eventSource = evt['EventSource']
+        if this_host in resources:
+            print("+ Scheduled Event. This host " + this_host +
+                " is scheduled for " + eventtype +
+                " by " + eventSource +
+                " with description " + description +
+                " not before " + notbefore)
+            # Add logic for handling events here
+
+
+def main():
+    data = get_scheduled_events()
+    handle_scheduled_events(data)
+
+
+if __name__ == '__main__':
+    main()
